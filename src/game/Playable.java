50a4cc98c7727178376973a64759f64395ffb67b
asvibar <asvibar@up.edu.ph>
2016-11-10 14:13:51 +0800

    Added Game Map (see Playable.java)

--- src/game/Playable.java
+++ src/game/Playable.java
@@ -1,7 +1,32 @@
+package game;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
 public class Playable extends JPanel{
 	private Map map;
+	private JFrame frame;
+	
+	public Playable(){
+		frame = new JFrame("T A N K E R M A N");
+		frame.setLayout(new BorderLayout());
+		map = new Map();
+		frame.add(map);
+		
+		frame.setSize(new Dimension(800,600));
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+		/*frame.addKeyListener(new KeyListener(){
+			public void keyPressed(KeyEvent e){
+				int keyCode = e.getKeyCode();
+				
+				if(keyCode == KeyEvent.VK_W){
+					
+				}
+			}
+		});*/
+	}
 }
--- src/game/TestMain.java
+++ src/game/TestMain.java
@@ -1,5 +1,7 @@
+package game;
+
 public class TestMain{
 	public static void main(String args[]){
-		Map test = new Map();
+		Playable test = new Playable();
 	}
 }
