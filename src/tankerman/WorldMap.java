0a6ab0923ef97413b55a6df774de714b4b7df3da
asvibar <asvibar@up.edu.ph>
2016-11-17 15:09:37 +0800

    Initial Bullet Commit

--- src/tankerman/WorldMap.java
+++ src/tankerman/WorldMap.java
@@ -1,7 +1,11 @@
 package tankerman;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 import org.newdawn.slick.*;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Vector2f;
 import org.newdawn.slick.state.*;
 import org.newdawn.slick.tiled.TiledMap;
 
@@ -12,6 +16,12 @@ public class WorldMap extends BasicGameState{
 	
 	int[] duration = {200,200};
 	
+	//Bullet Mechanics
+	private LinkedList<Bullet> bullets;
+	private static int FIRE_RATE = 250;
+	private int delta = 0;
+	private int current = 0;
+	
 	int charPositionX = 1;
 	int charPositionY = 2;
 	//float shiftX = charPositionX + 450;
@@ -21,9 +31,7 @@ public class WorldMap extends BasicGameState{
 	int cameraY = 0;
 	
 	int tileId;
-	
-	boolean[][] blocked;
-	
+
 	public WorldMap(int worldmap) {
 	}
 
@@ -40,7 +48,8 @@ public class WorldMap extends BasicGameState{
 		moveLeft = new Animation(walkLeft,duration,false);
 		moveRight = new Animation(walkRight,duration,false);
 		character = moveDown;
-		
+
+		bullets = new LinkedList<Bullet>();
 	}
 
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
@@ -48,12 +57,46 @@ public class WorldMap extends BasicGameState{
 		map.render(0,0);
 		character.draw(charPositionX * 30, charPositionY * 30);
 		g.drawString("CharacterX: " + charPositionX *30 + " CharY: " + charPositionY*30 , 400, 20);
+		
+		for(Bullet b : bullets){
+			b.render(gc, g);
+		}
 	}
 
-	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+	public void update(GameContainer gc, StateBasedGame sbg, int t) throws SlickException {
 		Input input = gc.getInput();
 		int objectLayer = map.getLayerIndex("Objects");
 		map.getTileId(0,0,objectLayer);
+		Iterator<Bullet> g = bullets.iterator();
+		
+		while(g.hasNext()){
+			Bullet b = g.next();
+			if(b.isActive() || (int)b.getPos().getX() < 700 || (int)b.getPos().getY() < 600){
+				b.update(t);
+			}else{
+				g.remove();
+			}
+			
+			if((int)b.getPos().getX() >= 720 || (int)b.getPos().getY() >= 570 || (int)b.getPos().getX() <= 35 || (int)b.getPos().getY() <= 35 ){
+				g.remove();
+			}
+			
+		}
+		
+		if(gc.getInput().isKeyPressed(Input.KEY_SPACE)){
+			if(character == moveDown){
+				bullets.add(new Bullet(new Vector2f((charPositionX*30) + 23,(charPositionY*30) + 30), new Vector2f(0,(charPositionY*30) + 60)));
+			}else if(character == moveUp){
+				bullets.add(new Bullet(new Vector2f((charPositionX*30) + 23,(charPositionY*30)), new Vector2f(0,-((charPositionY*30) - 60))));
+			}else if(character == moveLeft){
+				bullets.add(new Bullet(new Vector2f((charPositionX*30),(charPositionY*30) + 16), new Vector2f(-((charPositionX*30) - 60),0)));
+			}else if(character == moveRight){
+				bullets.add(new Bullet(new Vector2f((charPositionX*30)+30,(charPositionY*30) + 16), new Vector2f((charPositionX*30)+60,0)));
+			}
+			
+		}
+		
+		
 		if(input.isKeyPressed(Input.KEY_UP)){
 			character = moveUp;
 			charPositionY --;
